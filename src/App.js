433265187b6aea46daa7ed8447c2d757fdac46f4
AlenaNik <alena.nik.digital@gmail.com>
2020-01-08 22:41:26 +0100

    mid section finished

--- src/App.js
+++ src/App.js
@@ -48,14 +48,16 @@ function App() {
             look</h1>
     </header>
       <main role="main">
-        <h1>Redvolution is coming</h1>
-        <h2>Creamos una nueva marca que por fín refleja lo que llevamos haciendo con pasión toda la vida</h2>
-        <article className="article-left">
+          <h1 className="main-title"><strong> Red </strong> volution is coming</h1>
+        <h2 className="main-subtitle">Creamos una nueva marca que por fín refleja lo que llevamos haciendo con pasión toda la vida.</h2>
+           <div className="articles">
+          <article className="article-left">
           Team spirit and an inspiring working atmosphere are key. Reflecting this, we work with a team of UX and interactive designers, developers, project managers, digital consultants, thinkers and innovators all together in one space.
         </article>
         <article className="article-right">
           Digital consultants, thinkers and innovators all together in one space. In an environment that encourages flat hierarchies, identification and participation, bringing the digital world one step further.
         </article>
+           </div>
       </main>
   <section>
     <p className="clients-text-middle">
